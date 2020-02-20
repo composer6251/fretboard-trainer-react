64d689246de4c92c52af478b88c531bdfaeb2de5
David <dfennell31@gmail.com>
2020-02-20 17:24:35 -0500

    adding homepage-link css

--- src/components/homepage/homepage-link.component.jsx
+++ src/components/homepage/homepage-link.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import './homepage-link.styles.scss';
 
 
 const Homepage = () => (
 
-    <div id='homepage-component'>
-        <h2>Home</h2>
+    <div id='homepage-component' onClick='/'
+    >
+        <div>
+            <h2 id='sign-out'>Sign Out</h2>
+        </div>
+        <h2 id='homepage-link'>Home</h2>
     </div>
-
+    
 
 );
 
