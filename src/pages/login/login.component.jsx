c755c5f94211deb34a7eee73402002d681f56a25
David <dfennell31@gmail.com>
2020-05-06 10:01:27 -0400

    login hardcoded authentication

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,37 +1,61 @@
 import React, {Component} from 'react';
+
+import './login.styles.css';
+
 import HomepageLink from '../homepage/homepage.component';
 
 export default class LoginComponent extends Component{
     constructor(){
         super();
         this.state = {
-            username: "David Fennell",
-            password: "password"
+            username: "test",
+            password: "test",
+            loginFailed: false,
+            loginSuccessful: false
         }
-        this.handleUserNameChange = this.handleUserNameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.loginOnclick = this.loginOnclick.bind(this);
     }
-
-    handleUserNameChange(event){
+    //handle login un/pw input changes
+    handleChange(event){
         console.log(event.target.value);
-        this.setState({username: event.target.value});
+        this.setState({[event.target.name] : event.target.value});
     }
-    handlePasswordChange(event){
-        console.log(event.target.value);
-        this.setState({password: event.target.value});
+    //validate user info onClick
+    loginOnclick(event){
+        if(this.state.username === "test" && this.state.password === "test"){
+            this.setState({loginFailed : false});
+            this.setState({loginSuccessful : true});
+        }
+        else{
+            this.setState({loginFailed : true});
+            this.setState({loginSuccessful : false});
+        } 
     }
-
-
-
     render(){
         return(
             <div>
-                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUserNameChange}/>
-                Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
-                <button>Login</button>
+                <ShowFailureMessage loginFailed={this.state.loginFailed} />
+                <ShowSuccessMessage loginSuccessful={this.state.loginSuccessful} />
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                <button className="loginButton" onClick={this.loginOnclick}>Login</button>
             </div>
             
         )
     }
 }
 
+const ShowFailureMessage = (props) => {
+    if(props.loginFailed){
+        return <div>Invalid Credentials</div>
+    }
+    return null;
+}
+
+const ShowSuccessMessage = (props) => {
+    if(props.loginSuccessful){
+        return <div>Login Successful!</div>
+    }
+    return null;
+}
