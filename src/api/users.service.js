46e39088b205d80bebf70817bb137598e4b82dfb
David <dfennell31@gmail.com>
2020-06-04 08:58:03 -0400

    Swtiching PC

--- src/api/users.service.js
+++ src/api/users.service.js
@@ -6,20 +6,14 @@ class UsersService{
         return axios.get(`http://localhost:8080/users/${username}/userinfo`);
     }
     deleteUser(username, id){
-        //console.log(axios.get(`http://localhost:8080/users/${username}/userinfo/${id}`))
         return axios.delete(`http://localhost:8080/users/${username}/userinfo/${id}`);
     }
-<<<<<<< HEAD
     addUser(user){
         return axios.post(`http://localhost:8080/users/addUser`, user)
-=======
-    addUser(email, password){
-        console.log(email, password);
-        return axios.post(`http://localhost:8080/users/${email}/addUser/${password}`)
     }
+
     getUsers(){
         return axios.get(`http://localhost:8080/users`)
->>>>>>> d27c47c89160c0a4bac087be2aa768b1a66a9c4a
     }
     // updateUser(email, password){
     //     return axios.put(`http://localhost:8080/users/${email}/addUser/${password}`)
